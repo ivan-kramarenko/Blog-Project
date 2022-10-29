72d674ac2d6bc56cd9113317674bf8eab663db15
ivan-kramarenko <i-v-kr@yandex.ru>
2022-10-29 18:11:46 +0300

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        sss{" "}
+        </a>{" "}
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
