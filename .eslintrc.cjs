71fe84625826a060fab8dd2d67053f846dd266ee
ivan-kramarenko <i-v-kr@yandex.ru>
2022-10-29 19:05:44 +0300

    ready to work! (airbnb is legacy)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  plugins: ["react"],
-  rules: {},
-};
+  plugins: ['react'],
+  rules: {}
+}
