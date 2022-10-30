517f73ec152c135c91459da92c90688955164527
ivan-kramarenko <i-v-kr@yandex.ru>
2022-10-30 14:26:54 +0300

    add article file / add aliases to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from 'react'
+import ArticleItem from './features/article/articleItem/ArticleItem'
 import Navbar from './features/navbar/Navbar'
 
 const App = (): ReactElement => {
   return (
     <div className="App">
-      <Navbar />
+      <ArticleItem />
     </div>
   )
 }
