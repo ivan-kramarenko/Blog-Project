5eced26179f0ad4a3b1ecee9bd35dade27e6cc2b
ivan-kramarenko <i-v-kr@yandex.ru>
2022-10-30 14:34:30 +0300

    aliases

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import path from 'path'
 export default defineConfig({
   resolve: {
     alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@features': path.resolve(__dirname, './src/features'),
       '@utils': path.resolve(__dirname, './src/utils')
     }
   },
